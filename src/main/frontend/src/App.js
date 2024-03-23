612e9ded52fb03448ddb820d5ca5554c39b025b4
Jan Krunčík <kruncikjan95@gmail.com>
2024-03-23 23:45:58 +0100

    Návrh na Copyright

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import React, {useEffect, useState} from 'react';
+import {Route, Routes} from 'react-router-dom';
 import Navbar from './Navbar';
 import AboutProject from './AboutProject';
 import Profile from './Profile';
@@ -20,51 +18,50 @@ import ChangePassword from './ChangePassword';
 import i18next from 'i18next';
 
 function App() {
-  const { t } = useTranslation();
 
-  const locale = localStorage.getItem("Localization");
+    const locale = localStorage.getItem("Localization");
+    const [year] = useState(new Date().getFullYear());
 
+    useEffect(() => {
+        i18next.changeLanguage(locale)
+        localStorage.setItem("Localization", locale)
 
-  useEffect(() => {
-    i18next.changeLanguage(locale)
-    localStorage.setItem("Localization", locale)
+        if (locale === null) {
+            console.log("Changing language to czech");
+            i18next.changeLanguage("cs")
+            localStorage.setItem("Localization", "cs")
+        }
+    }, [locale])
 
-    if(locale === null) {
-      console.log("Changing language to czech");
-      i18next.changeLanguage("cs")
-      localStorage.setItem("Localization", "cs")
-     
-    }
-  }, [locale])
-  
- 
+    return (
+        <>
+            {Navbar()}
+            <div className='container'>
 
-  return (
-    <>
-      {Navbar()}
-      <div className='container'>
-        
-        <Routes>
-          <Route path="/aboutproject" element={<AboutProject />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/welcomePage" element={<WelcomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Registration />} />
-          <Route path="/UpdateData" element={<UpdateData />} />
-          <Route path="/addExercise" element={<AddExercise />} />
-          <Route path="/training" element={<Training />} />
-          <Route path="/generateTraining/:trainingType" element={<GenerateTraining />} />
-          <Route path="/exercise/:exerciseID" element={<Exercise />} />
-          <Route path="/adminTools" element={<AdminTools />} />
-          <Route path="/tokenExpired" element={<TokenExpired/>} />
-          <Route path="/changePassword" element={<ChangePassword/>} />
-          <Route path="*" element={<PageNotFound />} />
-          <Route path="/" element={<WelcomePage />} />
-        </Routes>
-      </div>
-      <footer> {t('footertext1')} <br/><br/> {t('copyright')}<a href='https://www.uhk.cz/cs/fakulta-informatiky-a-managementu/fim' target="_blank" rel="noopener noreferrer">UHK FIM</a> <br/>{t('copyright2')}</footer>
-    </>
-  );
+                <Routes>
+                    <Route path="/aboutproject" element={<AboutProject/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/welcomePage" element={<WelcomePage/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/register" element={<Registration/>}/>
+                    <Route path="/UpdateData" element={<UpdateData/>}/>
+                    <Route path="/addExercise" element={<AddExercise/>}/>
+                    <Route path="/training" element={<Training/>}/>
+                    <Route path="/generateTraining/:trainingType" element={<GenerateTraining/>}/>
+                    <Route path="/exercise/:exerciseID" element={<Exercise/>}/>
+                    <Route path="/adminTools" element={<AdminTools/>}/>
+                    <Route path="/tokenExpired" element={<TokenExpired/>}/>
+                    <Route path="/changePassword" element={<ChangePassword/>}/>
+                    <Route path="*" element={<PageNotFound/>}/>
+                    <Route path="/" element={<WelcomePage/>}/>
+                </Routes>
+            </div>
+
+            <footer> © {year} - <a href='https://www.uhk.cz/cs/fakulta-informatiky-a-managementu/fim' target="_blank"
+                                   rel="noopener noreferrer">UHK FIM</a> - All Rights Reserved.
+            </footer>
+        </>
+    );
 }
 
 export default App;
